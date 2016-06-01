588806fe83d58cbc1cd6e9bb7f3265fd21b194cc
Nathan Azevedo <dev.nathan.guimaraes@gmail.com>
2016-06-01 11:38:08 -0300

    adding more tweaks configuration

--- git_configs.sh
+++ git_configs.sh
@@ -12,11 +12,17 @@ git config --global push.default simple
 echo "Configuring pull with '--rebase' default.."
 git config --global pull.rebase true
 
-echo "Configuring 'rerere' for automatic rebasing between branches.."
+echo "Configuring 'rerere' for reused recorded resolution(less merging).."
 git config --global rerere.enabled true
 
 echo "Configuring Colored UI for git .."
 git config --global color.ui true
+
+echo "Configuring alias 's' for 'status -s' .."
+git config alias.s "status -s"
+
+echo "Configuring alias 'lg' for 'log decoration' .."
+git config alias.lg "log --oneline --decoration --all --graph"
 
 echo "End of basic configuration.."
 
